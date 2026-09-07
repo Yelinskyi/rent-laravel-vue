8f40981b45a1fe1074b63a6c4a443ec2b4bc745e
vitaliiyelinskyi <vitaliiyelinskyi@gmail.com>
2026-09-07 16:48:30 -0500

    inertia add client side setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import inertia from "@inertiajs/vite";
 
 export default defineConfig({
     plugins: [
@@ -16,7 +17,7 @@ export default defineConfig({
             ],
         }),
         tailwindcss(),
-        vue({
+        vue({ //back end side
             template: {
                 transformAssetUrls: {
                     // The Vue plugin will re-write asset URLs, when referenced
@@ -34,6 +35,7 @@ export default defineConfig({
                 },
             },
         }),
+        inertia(), // front end side
     ],
     server: {
         watch: {
